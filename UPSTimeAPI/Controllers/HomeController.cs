3899ff609019181993b2b7327ce35f1f0f9ad823
swonb <swonbinca@gmail.com>
2019-08-28 19:31:44 -0400

    Initial Commit
    Display Service Code, Name and Date

--- UPSTimeAPI/Controllers/HomeController.cs
+++ UPSTimeAPI/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UPSTimeAPI.Models;
+using UPSTimeAPI.ServiceReference3;
 
 namespace UPSTimeAPI.Controllers
 {
@@ -10,7 +12,76 @@ namespace UPSTimeAPI.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            TimeInTransitService tntService = new TimeInTransitService();
+            TimeInTransitRequest tntRequest = new TimeInTransitRequest();
+            RequestType request = new RequestType();
+            String[] requestOption = { "TNT" };
+            request.RequestOption = requestOption;
+            tntRequest.Request = request;
+
+            RequestShipFromType shipFrom = new RequestShipFromType();
+            RequestShipFromAddressType addressFrom = new RequestShipFromAddressType();
+            addressFrom.City = "Toronto";
+            addressFrom.CountryCode = "CA";
+            addressFrom.PostalCode = "M1P4P5";
+            //addressFrom.StateProvinceCode = "ShipFrom state province code";
+            shipFrom.Address = addressFrom;
+            tntRequest.ShipFrom = shipFrom;
+
+            RequestShipToType shipTo = new RequestShipToType();
+            RequestShipToAddressType addressTo = new RequestShipToAddressType();
+            addressTo.City = "Toronto";
+            addressTo.CountryCode = "CA";
+            addressTo.PostalCode = "M1P4P5";
+            //addressTo.StateProvinceCode = "ShipTo state province code";
+            shipTo.Address = addressTo;
+            tntRequest.ShipTo = shipTo;
+
+            PickupType pickup = new PickupType();
+            pickup.Date = "20190830";
+            tntRequest.Pickup = pickup;
+
+            //Below code uses dummy data for reference. Please update as required.
+            ShipmentWeightType shipmentWeight = new ShipmentWeightType();
+            shipmentWeight.Weight = "10";
+            CodeDescriptionType unitOfMeasurement = new CodeDescriptionType();
+            unitOfMeasurement.Code = "KGS";
+            unitOfMeasurement.Description = "Kilograms";
+            shipmentWeight.UnitOfMeasurement = unitOfMeasurement;
+            tntRequest.ShipmentWeight = shipmentWeight;
+
+            tntRequest.TotalPackagesInShipment = "1";
+            InvoiceLineTotalType invoiceLineTotal = new InvoiceLineTotalType();
+            invoiceLineTotal.CurrencyCode = "CAD";
+            invoiceLineTotal.MonetaryValue = "10";
+            tntRequest.InvoiceLineTotal = invoiceLineTotal;
+            tntRequest.MaximumListSize = "1";
+
+            UPSSecurity upss = new UPSSecurity();
+            UPSSecurityServiceAccessToken upsSvcToken = new UPSSecurityServiceAccessToken();
+            upsSvcToken.AccessLicenseNumber = "3D6A1DD5F39023B5";
+            upss.ServiceAccessToken = upsSvcToken;
+            UPSSecurityUsernameToken upsSecUsrnameToken = new UPSSecurityUsernameToken();
+            upsSecUsrnameToken.Username = "Deverloper2019";
+            upsSecUsrnameToken.Password = "Deverloper=2019";
+            upss.UsernameToken = upsSecUsrnameToken;
+            tntService.UPSSecurityValue = upss;
+
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12 | System.Net.SecurityProtocolType.Tls | System.Net.SecurityProtocolType.Tls11; //This line will ensure the latest security protocol for consuming the web service call.
+            Console.WriteLine(tntRequest);
+            TimeInTransitResponse tntResponse = tntService.ProcessTimeInTransit(tntRequest);
+
+            Console.WriteLine("Response code: " + tntResponse.Response.ResponseStatus.Code);
+            Console.WriteLine("Response description: " + tntResponse.Response.ResponseStatus.Description);
+
+            TransitResponseType responseItem = (TransitResponseType)tntResponse.Item;
+
+            RatePackage model = new RatePackage()
+            {
+                timeResponse = responseItem
+            };
+            
+            return View(model);
         }
 
         public ActionResult About()
---
+++ UPSTimeAPI/Models/RatePackage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UPSTimeAPI.ServiceReference3;
+
+namespace UPSTimeAPI.Models
+{
+    public class RatePackage
+    {
+        public TransitResponseType timeResponse { get; set; }
+    }
+}
